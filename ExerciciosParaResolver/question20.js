f8189b1441baef445688f5aa8ae9a531a1f82a3e
Ruan L. Andrade <ruanlandrade@outlook.com>
2021-10-13 21:48:00 -0300

    New Questions

--- ExerciciosParaResolver/question20.js
+++ ExerciciosParaResolver/question20.js
@@ -4,17 +4,73 @@ const sacarDinheiro = function (valorSaque) {
   let cont10 = 0
   let cont5 = 0
   let cont1 = 0
+  let valorNota = calcularValorNota(valorSaque)
 
-  switch (valor) {
-    case valor > 100 :
-      return console.log(`${valor % 100} nota de 100`)
+  while(valorSaque >= valorNota) {
+    switch (valorNota) {
+      case 100 :
+        valorSaque -= 100
+        cont100++
+        break
+      case 50 : 
+        valorSaque -= 50
+        cont50++
+        break
+      case 10 :
+        valorSaque -= 10
+        cont10++
+          break
+      case 5 :
+        valorSaque -= 5
+        cont5++
+        break
+      case 1 : 
+        valorSaque -= 1
+        cont1++
+        break
+    }
+    valorNota = calcularValorNota(valorSaque)
   }
+  return elaborarResultado(cont100, cont50, cont10, cont5, cont1)
 }
 
 const calcularValorNota = function (valorSaque) {
-  a 
+  if(valorSaque >= 100) {
+    return 100
+  } else if (valorSaque >= 50) {
+    return 50
+  } else if (valorSaque >= 10) {
+    return 10
+  } else if ( valorSaque >= 5) {
+    return 5
+  } else if ( valorSaque >= 1) {
+    return 1
+  }
 }
 
 const elaborarResultado = function (cont100, cont50, cont10, cont5, cont1) {
-  
+ let resultado = ''
+ 
+ if(cont100 > 0 ) {
+   resultado += `${cont100} nota(s) de R$100`
+ }
+
+ if (cont50 > 0 ) {
+   resultado += `\n${cont50} nota(s) de R$50`
+ }
+
+ if(cont10 > 0 ) {
+   resultado += `\n${cont10} nota(s) de R$10`
+ }
+
+ if(cont5 > 0) {
+   resultado += `\n${cont5} nota(s) de R$5`
+ }
+
+ if(cont1 > 0) {
+   resultado += `\n${cont1} nota(s) de R$1`
+ }
+ return resultado
 }
+
+console.log(sacarDinheiro(283))
--- ExerciciosParaResolver/question22.js
+++ ExerciciosParaResolver/question22.js
@@ -1,8 +1,10 @@
-let valueMembership = function (mes, valor) {
-  switch (mes){
-    case 1 :
-      return valor = valor * 1.05
-    break
-    
+let valueMembership = function (month, value) {
+  if (month > 0 && month < 13){
+    delayed = month - 1
+    return console.log(`${value * (1.05**delayed).toFixed(2)}`)
+  } else {
+    return 'Invalid month'
   }
 }
+
+valueMembership(4, 100)
